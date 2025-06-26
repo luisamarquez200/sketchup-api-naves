48b3f20d27ef3d8aa561608326a2d457572f5db9
luisamarquez200 <luisamarquez2008@gmail.com>
2025-06-25 22:27:20 -0500

    feat: ajuste de los estados de montacargas

--- controllers/forkliftController.js
+++ controllers/forkliftController.js
@@ -5,23 +5,24 @@ exports.getSububicacionesByCuadrante = async (req, res) => {
   const cuadrante = req.params.nombre;
 
   const query = `
-  SELECT 
-    su.nombre AS sub_ubicacion,
-    su.ubicacion_ocupada AS ocupada,
-    eu.id_equipos AS equipo_id,
-    e.estado
-  FROM sub_ubicaciones su
-  LEFT JOIN equipo_ubicacion eu ON su.id_sub_ubicacion = eu.id_sub_ubicacion
-  LEFT JOIN equipos e ON eu.id_equipos = e.id_equipos
-  WHERE su.id_ubicacion = (
-    SELECT id_ubicacion FROM ubicacion WHERE nombre_ubicacion = ?
-  )
-`;
+    SELECT 
+      su.nombre AS sub_ubicacion,
+      su.ubicacion_ocupada AS ocupada,
+      eu.id_equipos AS equipo_id,
+      e.estado
+    FROM sub_ubicaciones su
+    LEFT JOIN equipo_ubicacion eu ON su.id_sub_ubicacion = eu.id_sub_ubicacion
+    LEFT JOIN equipos e ON eu.id_equipos = e.id_equipos
+    WHERE su.id_ubicacion = (
+      SELECT id_ubicacion FROM ubicacion WHERE nombre_ubicacion = ?
+    )
+  `;
 
-
-
-  db.query(query, [cuadrante], (err, results) => {
-    if (err) return res.status(500).json({ error: err.message });
+  try {
+    const [results] = await db.query(query, [cuadrante]);
     res.json(results);
-  });
+  } catch (err) {
+    console.error('Error en getSububicacionesByCuadrante:', err);
+    res.status(500).json({ error: err.message });
+  }
 };
